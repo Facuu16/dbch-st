f171e34a63c14fcbce18eaffd398fd832dbd7a40
Facuu16 <hincfacundo@gmail.com>
2023-05-09 22:40:45 -0300

    GUI changes

--- DBCH.STool/PromptGUI.Designer.cs
+++ DBCH.STool/PromptGUI.Designer.cs
@@ -53,7 +53,7 @@
             this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Image = global::DBCH.STool.Properties.Resources.tick_yes;
+            this.button1.Image = global::DBCH.STool.Properties.Resources.yes;
             this.button1.Location = new System.Drawing.Point(12, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(49, 29);
@@ -67,7 +67,7 @@
             this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.Image = global::DBCH.STool.Properties.Resources.tick_no;
+            this.button2.Image = global::DBCH.STool.Properties.Resources.no;
             this.button2.Location = new System.Drawing.Point(223, 77);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(49, 29);
